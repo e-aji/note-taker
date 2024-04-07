12c05828c887a894e4a9b20c9f1baf396e855d0b
eji_a <156595423+e-aji@users.noreply.github.com>
2024-04-07 15:10:02 +0100

    updates to code to have post data working

--- routes/notes.js
+++ routes/notes.js
@@ -1,49 +1,48 @@
 const notes = require('express').Router();
-const uuid = require('../helpers/uuid');
-const { readFromFile, readAndAppend, writeToFile } = require('../helpers/fsUtils');
+const { v4: uuidv4 } = require('uuid');
+const { readFromFile, readAndAppend, writeToFile } = require('../helpers/fsUtils.js');
 
 notes.get('/', (req, res) => {
-    // console.info(`${req.method} request received for notes`);
+    console.info(`${req.method} Request received for notes`);
     readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
 
-// notes.get ('/notes' , (req, res) => {
-//     const noteId = req.params;
-//     readFromFile('./db/db.json')
-//         .then((data) => JSON.parse(data))
-//         .then((json) => {
-//             const result = json.filter((note) => note.id === noteId);
-//             return result.length > 0
-//                 ? res.json(result)
-//                 : res.json('No note with that ID');
-//         });
-// });
-
-notes.post('./', (req, res) => {
-    const { title, text } = req.body;
-    if (title && text) {
-        const newNotes = {
-            title,
-            text,
-            id: uuid(),
-        };
-        readAndAppend(newNotes, './db/db.json');
-        res.json(`Note successfully added!`);
-    } else {
-        res.error('Error adding note');
-    }
-}); 
-
-notes.delete('/:id', (req, res) => {
-    const id = req.params.id;
+notes.get ('/:notes_id' , (req, res) => {
+    const noteId = req.params.notes_id;
     readFromFile('./db/db.json')
         .then((data) => JSON.parse(data))
         .then((json) => {
+            const result = json.filter((note) => note.id === noteId);
+            return result.length > 0
+                ? res.json(result)
+                : res.json('No note with that ID');
+        });
+});
 
-            // sort this out 
-            const result = json.filter((note) => id !== id);
+notes.post('/', (req, res) => {
+    const { title, text } = req.body;
+    if (title && text) {
+        const newNote = {
+            title,
+            text,
+            id: uuidv4(),
+        };
+        readAndAppend(newNote, './db/db.json');
+        res.json(`Note successfully added!`);
+    } else {
+        res.status(400).json('Error adding note, title and text are required');
+    }
+}); 
+
+notes.delete('/:notes_id', (req, res) => {
+    const noteId = req.params.note_id;
+    readFromFile('./db/db.json')
+        .then((data) => JSON.parse(data))
+        .then((json) => {
+            const result = json.filter((note) => note.id !== noteId);
+
             writeToFile('./db/db.json', result);
-            res.json(`Item ${id} has been deleted 🗑️`);
+            res.json(`Item ${noteId} has been deleted 🗑️`);
         });
 });
 
