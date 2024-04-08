6a11fea37b8b058d728544c89d02790cd3702ca3
eji_a <156595423+e-aji@users.noreply.github.com>
2024-04-08 13:32:41 +0100

    changes made to code to make sure response is valid json

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,6 @@ const express = require('express');
 const notesRouter = require('./notes');
 const app = express();
 
-app.use('./notes', notesRouter);
+app.use('/notes', notesRouter);
 
 module.exports = app;
--- server.js
+++ server.js
@@ -20,9 +20,9 @@ app.get('/notes', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
-app.get('*', (req, res) =>
-    res.sendFile(path.join(__dirname, '/public/index.html'))
-);
+// app.get('*', (req, res) =>
+//     res.sendFile(path.join(__dirname, '/public/index.html'))
+// );
 
 
 app.listen(PORT, () =>
